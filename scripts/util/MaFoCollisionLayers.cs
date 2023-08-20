90f04aba02d649f8d31d5b5cc2bfb1937654ae10
ChloePrime <chloe_von_einzbern@qq.com>
2023-08-20 19:08:35 +0800

    让马里奥不再是实心的

--- scripts/util/MaFoCollisionLayers.cs
+++ scripts/util/MaFoCollisionLayers.cs
@@ -3,6 +3,10 @@
 public static class MaFoCollisionLayers
 {
     public const int Solid = 1;
+    public const int SolidMarioOnly = 2;
+    public const int SolidEnemyOnly = 4;
     public const int DamageSource = 1 << 8;
     public const int DeathSource = 1 << 9;
+    public const int Mario = 1 << 16;
+    public const int Enemy = 1 << 17;
 }
