f0a6e5664af7f2a0b97d1b48be7758da4a0193f9
ChloePrime <chloe_von_einzbern@qq.com>
2023-08-26 18:46:03 +0800

    优化 GameRule 获取

--- scripts/GameRule.cs
+++ scripts/GameRule.cs
@@ -1,19 +1,34 @@
-﻿using ChloePrime.MarioForever.Util;
+﻿using System.Linq;
+using ChloePrime.MarioForever.Level;
+using ChloePrime.MarioForever.Util;
+using DotNext;
 using Godot;
+using MixelTools.Util.Extensions;
 
 namespace ChloePrime.MarioForever;
 
 public partial class GameRule : Resource
 {
     public static GameRule Default => _default ??= GD.Load<GameRule>("res://R_game_rule.tres");
+    public static GameRule Get() => GetManager(Engine.GetMainLoop() as SceneTree)?.GameRule ?? Default;
     
     [Export] public bool DisableLives { get; set; }
     [Export] public bool DisableScore { get; set; }
 
+    private static Optional<LevelManager> _manager;
     private static GameRule _default;
+
+    private static LevelManager GetManager(SceneTree tree)
+    {
+        if (_manager.IsUndefined)
+        {
+            _manager = tree?.Root.Walk().OfType<LevelManager>().First();
+        }
+        return _manager.OrDefault();
+    }
 }
 
 public static class GameRuleEx
 {
-    public static GameRule GetRule(this Node node) => node.GetLevelManager()?.GameRule ?? GameRule.Default;
+    public static GameRule GetRule(this Node node) => node.GetLevelManager()?.GameRule ?? GameRule.Get();
 }
