617bc15016413589b9fec1776d52ddf7600e478e
ChloePrime <chloe_von_einzbern@qq.com>
2023-09-14 19:42:12 +0800

    地鼠Tile

--- tiles/OBJ_TS_enemies.tsx
+++ tiles/OBJ_TS_enemies.tsx
@@ -1,10 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="OBJ_TS_enemies" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="OBJ_TS_enemies" tilewidth="32" tileheight="32" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0" x="1" y="0" width="32" height="32" type="instance">
+ <tile id="0" x="0" y="0" width="31" height="32" type="instance">
   <properties>
    <property name="res_path" type="file" value="res://objects/enemies/O_goomba.tscn"/>
   </properties>
-  <image width="1712" height="1088" source="../resources/enemies/T_enemies.png"/>
+  <image width="31" height="96" source="../resources/enemies/T_goomba.png"/>
+ </tile>
+ <tile id="1" x="32" y="0" width="32" height="32" type="instance">
+  <properties>
+   <property name="res_path" type="file" value="res://objects/enemies/O_mole.tscn"/>
+  </properties>
+  <image width="64" height="32" source="../resources/enemies/T_mole.png"/>
+ </tile>
+ <tile id="2" x="0" y="0" width="32" height="32" type="instance">
+  <properties>
+   <property name="res_path" type="file" value="res://objects/enemies/O_mole_smb3.tscn"/>
+  </properties>
+  <image width="96" height="64" source="../resources/enemies/T_mole_smb3.png"/>
  </tile>
 </tileset>
