3deef383f8145209cdce2c37d98086d3cf89e7f0
ChloePrime <chloe_von_einzbern@qq.com>
2024-07-15 00:02:50 +0800

    背景土块（小块云暂未实现）

---
+++ tiles/TS_scenery.tsx
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TS_scenery" tilewidth="32" tileheight="32" tilecount="490" columns="35">
+ <image source="T_scenery.png" width="1120" height="448"/>
+ <tile id="141">
+  <animation>
+   <frame tileid="141" duration="143"/>
+   <frame tileid="176" duration="143"/>
+   <frame tileid="211" duration="143"/>
+  </animation>
+ </tile>
+ <tile id="142">
+  <animation>
+   <frame tileid="142" duration="143"/>
+   <frame tileid="177" duration="143"/>
+   <frame tileid="212" duration="143"/>
+  </animation>
+ </tile>
+ <tile id="214">
+  <animation>
+   <frame tileid="214" duration="143"/>
+   <frame tileid="249" duration="143"/>
+   <frame tileid="284" duration="143"/>
+   <frame tileid="319" duration="143"/>
+  </animation>
+ </tile>
+ <tile id="215">
+  <animation>
+   <frame tileid="215" duration="143"/>
+   <frame tileid="250" duration="143"/>
+   <frame tileid="285" duration="143"/>
+   <frame tileid="320" duration="143"/>
+  </animation>
+ </tile>
+ <tile id="216">
+  <animation>
+   <frame tileid="216" duration="143"/>
+   <frame tileid="251" duration="143"/>
+   <frame tileid="286" duration="143"/>
+   <frame tileid="321" duration="143"/>
+  </animation>
+ </tile>
+</tileset>
