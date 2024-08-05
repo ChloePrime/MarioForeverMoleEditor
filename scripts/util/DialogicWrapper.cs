296a480fd90d9881ce6f388922188b35f13c56ba
ChloePrime <chloe_von_einzbern@qq.com>
2024-08-05 23:08:40 +0800

    移除Dialogic

---
+++ scripts/util/DialogicWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using ChloePrime.Godot.Util;
+using Godot;
+
+namespace ChloePrime.MarioForever.Util;
+
+public class DialogicWrapper
+{
+    public static Node Instance => _instance.Value;
+    
+    // public static void Start()
+    
+    private static readonly Lazy<Node> _instance = new(FindSingleton);
+
+    [return: MaybeNull]
+    private static Node FindSingleton()
+    {
+        return Engine.GetMainLoop() is SceneTree tree 
+            ? tree.Root.Children().First(node => node.Name == (StringName)"Dialogic")
+            : null;
+    }
+}
