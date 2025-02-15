aa0c08819278a86ee86168f060ec35623359ebdf
Chloe Prime <chloe_von_einzbern@qq.com>
2025-02-15 14:55:50 +0800

    16x版的SMB1 TileSet

---
+++ engine/tiles/smb1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="smb1" tilewidth="32" tileheight="32" spacing="2" tilecount="768" columns="24">
+ <image source="smb1.png" width="814" height="1086"/>
+</tileset>
