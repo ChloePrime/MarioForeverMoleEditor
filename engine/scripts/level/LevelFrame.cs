1ce8aba2f1997e79bdc71ef9fa051ea914f3830d
Chloe Prime <chloe_von_einzbern@qq.com>
2025-02-18 19:34:15 +0800

    Level Frame V3: 将原始分辨率调回640×480，让Level Container使用XBR Shader， 将清晰度提升到前所未有的等级！

--- engine/scripts/level/LevelFrame.cs
+++ engine/scripts/level/LevelFrame.cs
@@ -1,70 +1,30 @@
 using ChloePrime.Godot.Util;
-using ChloePrime.MarioForever.Util;
 using Godot;
 
 namespace ChloePrime.MarioForever.Level;
 
 public partial class LevelFrame : LevelManager
 {
-    [Export]
-    public bool UseFilter
-    {
-        get => _useFilter;
-        set => SetUseFilter(value);
-    }
-
-    [Export] 
-    public Material FilterMaterial { get; private set; } = GD.Load<Material>("res://engine/resources/level/super_sai.mtl.tres");
-    
-    
     [ExportGroup("Advanced")]
     [Export] private Control LevelContainer { get; set; }
     [Export] private SubViewport LevelViewport { get; set; }
-    
-    private Vector2I LevelViewportInitialSize { get; set; }
 
     public override void _Ready()
     {
         Main.Init();
         base._Ready();
-        this.GetNode(out _la, NpLeftAnimFrame);
+        this.GetNode(out _la, NpLeftAnimFrame); 
         this.GetNode(out _ra, NpRightAnimFrame);
         this.GetNode(out _ln, NpLeftNormalFrame);
         this.GetNode(out _rn, NpRightNormalFrame);
         this.GetNode(out _game, NpGameContainer);
         GetWindow().ContentScaleAspect = Window.ContentScaleAspectEnum.Expand;
         GetWindow().SizeChanged += OnResize;
-        _ready.Value = true;
-        LevelViewportInitialSize = LevelViewport.Size;
-        SetUseFilter(UseFilter);
-    }
-
-    public override void _Input(InputEvent e)
-    {
-        base._Input(e);
-        if (e is InputEventKey { PhysicalKeycode: Key.F6, Pressed: true })
-        {
-            UseFilter = !UseFilter;
-        }
-    }
-
-    private bool SetUseFilter(bool value)
-    {
-        if (!_ready.Value)
-        {
-            goto ret;
-        }
-
-        LevelContainer.Material = value ? FilterMaterial : null;
-        LevelViewport.Size = value ? LevelViewportInitialSize / 2 : LevelViewportInitialSize;
-        
-        ret:
-        return _useFilter = value;
     }
 
     private void OnResize()
     {
-        var sizeRate = _game.Position.X / this.Size.X;
+        var sizeRate = _game.Position.X / Size.X;
         _la.AnchorLeft = _ln.AnchorLeft = -sizeRate;
         _ra.AnchorRight = _rn.AnchorRight = 1 + sizeRate;
         _la.Position = _ln.Position = new Vector2(+0.5F, _ra.Position.Y);
@@ -79,6 +39,4 @@ public partial class LevelFrame : LevelManager
     private Control _ln, _rn;
     private Control _la, _ra;
     private AspectRatioContainer _game;
-    private UnserializedContainer<bool> _ready;
-    private bool _useFilter;
 }
